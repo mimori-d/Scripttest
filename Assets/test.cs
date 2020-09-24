54dbe69b795c93f3962748b5deefcb510787ec98
daichimimori <mimori-d@uniqrazy.com>
2020-09-24 14:56:23 +0900

    ScriptTestプロジェクト 初めてのコミット
    Lesson4課題提出となります。

---
+++ Assets/test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 配列を初期化する
+		int[] points = {1, 2, 3, 4, 5};
+
+		// 5回処理を繰り返す
+		for (int i = 0; i < 5; i++) {
+			Debug.Log (points [i]);
+		}
+
+		// 5回処理を繰り返す
+		for (int i = 4; i >=  0; i--) {
+			Debug.Log (points [i]);
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
